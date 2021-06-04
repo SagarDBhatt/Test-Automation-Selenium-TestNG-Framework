19f8db0edb04da36355a152c60a24352364309cf
Bhatt, Sagar <sagar.bhatt@dish.com>
2021-06-03 21:32:28 -0600

    ExcelUtilities using Apache POI API.

---
+++ src/main/java/ExcelUtils/ExcelConfigurationFile/ExcelConf.java
@@ -0,0 +1,22 @@
+package ExcelUtils.ExcelConfigurationFile;
+
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+
+public class ExcelConf {
+
+    public static void getRowCount() throws IOException {
+        System.out.println("Inside the method");
+        XSSFWorkbook workbook = new XSSFWorkbook("C:\\Users\\sagar.bhatt\\IdeaProjects\\Test-Automation-Selenium-TestNG-Framework\\src\\main\\java\\ExcelUtils\\ExcelConfigurationFile\\confData.xlsx");
+        XSSFSheet sheet = workbook.getSheet("Sheet1");
+        System.out.println(sheet.getLastRowNum());
+    }
+
+    public static void main(String[] args) throws IOException {
+        getRowCount();
+    }
+
+}
