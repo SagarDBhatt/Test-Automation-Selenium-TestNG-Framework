c20f00b53f986b64d28e5b82a01a0ee954a9cd98
Bhatt, Sagar <sagar.bhatt@dish.com>
2021-06-07 15:30:24 -0600

    DataFormatter to get the string value of each cell (Resolve the exception of INT cell value)

--- src/main/java/ExcelUtils/ExcelConfigurationFile/ExcelConf.java
+++ src/main/java/ExcelUtils/ExcelConfigurationFile/ExcelConf.java
@@ -1,5 +1,8 @@
 package ExcelUtils.ExcelConfigurationFile;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -7,16 +10,47 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import java.io.IOException;
 
 public class ExcelConf {
+    static XSSFWorkbook workbook;
+    static XSSFSheet sheet;
+    static DataFormatter formatter = new DataFormatter();
 
+    /**
+     * Method to access the Excel sheet and get the row counts.
+     * @throws IOException
+     */
     public static void getRowCount() throws IOException {
 //        System.out.println("Inside the method");
-        XSSFWorkbook workbook = new XSSFWorkbook("C:\\Users\\sagar.bhatt\\IdeaProjects\\Test-Automation-Selenium-TestNG-Framework\\src\\main\\java\\ExcelUtils\\ExcelConfigurationFile\\confData.xlsx");
-        XSSFSheet sheet = workbook.getSheet("Sheet1");
+        workbook = new XSSFWorkbook("C:\\Users\\sagar.bhatt\\IdeaProjects\\Test-Automation-Selenium-TestNG-Framework\\src\\main\\java\\ExcelUtils\\ExcelConfigurationFile\\confData.xlsx");
+        sheet = workbook.getSheet("Sheet1");
+
+        /**
+         * In Excel index starts with 0. Last Row count = 3 suggests that there are 4 entries with index 0,1,2,3.
+         * Total entries can be found by "getPhysicalNumberOfRows()" method.
+         */
         System.out.println("Last row " + sheet.getLastRowNum());
         System.out.println("Pysical Count = " + sheet.getPhysicalNumberOfRows());
+
+        for (int i=0; i <= sheet.getLastRowNum(); i++){
+            Row r = sheet.getRow(i);
+
+            for (int j = 0; j < r.getLastCellNum(); j++){
+                System.out.println(formatter.formatCellValue(getCellData(i,j)));
+            }
+        }
     }
 
+    /**
+     * Method to access the cell value. Method takes rowNumber and ColNumber as an argument and return the cell value.
+     * @param rowNumber
+     * @param columnNumber
+     */
+    public static Cell getCellData(int rowNumber, int columnNumber){
 
+//        String cellData = sheet.getRow(rowNumber).getCell(columnNumber).getStringCellValue();
+        Row row = sheet.getRow(rowNumber);
+        Cell cell = row.getCell(columnNumber);
+        return cell;
+    }
 
     public static void main(String[] args) throws IOException {
         getRowCount();
