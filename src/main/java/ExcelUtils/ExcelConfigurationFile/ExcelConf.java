8c83b9acf2299e97852c31299c3dae681e599e07
Bhatt, Sagar <sagar.bhatt@dish.com>
2021-06-07 11:01:58 -0600

    ExcelUtilities using Apache POI API. Get the Row Counts

--- src/main/java/ExcelUtils/ExcelConfigurationFile/ExcelConf.java
+++ src/main/java/ExcelUtils/ExcelConfigurationFile/ExcelConf.java
@@ -9,11 +9,14 @@ import java.io.IOException;
 public class ExcelConf {
 
     public static void getRowCount() throws IOException {
-        System.out.println("Inside the method");
+//        System.out.println("Inside the method");
         XSSFWorkbook workbook = new XSSFWorkbook("C:\\Users\\sagar.bhatt\\IdeaProjects\\Test-Automation-Selenium-TestNG-Framework\\src\\main\\java\\ExcelUtils\\ExcelConfigurationFile\\confData.xlsx");
         XSSFSheet sheet = workbook.getSheet("Sheet1");
-        System.out.println(sheet.getLastRowNum());
+        System.out.println("Last row " + sheet.getLastRowNum());
+        System.out.println("Pysical Count = " + sheet.getPhysicalNumberOfRows());
     }
+
+
 
     public static void main(String[] args) throws IOException {
         getRowCount();
