8133d30410e80d3308211caebe088f1f849cff8f
Bhatt, Sagar <sagar.bhatt@dish.com>
2021-06-14 17:05:04 -0600

    Login to NXO and acknowledge the pop up.

--- src/main/java/NXO_TC_Demo/BOM_ID_Validation.java
+++ src/main/java/NXO_TC_Demo/BOM_ID_Validation.java
@@ -8,14 +8,18 @@ import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import javax.swing.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
+
 
 public class BOM_ID_Validation {
     WebDriver driver = createWebdriver();
@@ -50,12 +54,10 @@ public class BOM_ID_Validation {
     @Test()
     public void loginPage() throws IOException, InterruptedException {
 
-//        WebDriver driver = createWebdriver();
-
         driver.get("https://dishuat.nexsysone.com/nexsysonev3/");
         driver.manage().timeouts().implicitlyWait(8000, TimeUnit.MILLISECONDS);
         driver.manage().window().maximize();
-        takeScreenShot(driver,"loginScreen");
+//        takeScreenShot(driver,"loginScreen");
 
     // Below code can be done
 //        WebElement OktaTileElement = driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[4]/div[2]/a"));
@@ -64,6 +66,9 @@ public class BOM_ID_Validation {
         driver.findElement(By.xpath("//*[@id=\"loginForm\"]/div[4]/div[2]/a")).click();
         Thread.sleep(2000);
         takeScreenShot(driver,"DISH_OKTA_Login");
+
+        Thread.sleep(4000);
+        takeScreenShot(driver,"HomePage");
 
         /**
          * Below code commented. When connected via LAN cable, page redirects to OKTA login page,
@@ -98,45 +103,47 @@ public class BOM_ID_Validation {
     }
 
     @Test(dependsOnMethods = {"loginPage"})
-    public void getBOMId() throws InterruptedException {
+    public void getBOMId() throws InterruptedException, IOException {
 
         /**
          * Here, I need to manage pop up window. Selenium needs to switch the workflow to new pop up
          * window and acknowledge. After that I need to switch back to parent window element to Resume the
          * workflow.
          */
-        Thread.sleep(10000);
-        String parentWindowHandler = driver.getWindowHandle();
-        String popUpWindowHandler = null;
+//        Thread.sleep(10000);
+//        String popUpWindowHandler = null;
+//
+//        Set<String> handles = driver.getWindowHandles();
+//        Iterator<String> iterator = handles.iterator();
+//
+//        while(iterator.hasNext()){
+//            popUpWindowHandler = iterator.next();
+//            System.out.println("Elements = " + popUpWindowHandler);
+//        }
+//
+//        driver.switchTo().window(popUpWindowHandler);
+//        Thread.sleep(2000);
 
-        Set<String> handles = driver.getWindowHandles();
-        Iterator<String> iterator = handles.iterator();
-
-        while(iterator.hasNext()){
-            popUpWindowHandler = iterator.next();
-        }
-
-        driver.switchTo().window(popUpWindowHandler);
-        Thread.sleep(2000);
-
-        System.out.println("Before pop up screnn");
-        driver.findElement(By.xpath("//*[@id=\"575da21e-5ab0-54bf-99f8-86cf03a88669\"]")).click();
-        System.out.println("After pop up screen");
-//        System.out.println("Parent Window = " + parentWindowHandler + "Pop up window = " + popUpWindowHandler);
-
-        Thread.sleep(2000);
-        driver.switchTo().window(parentWindowHandler);
-
-        System.out.println("Before inserting the value");
-        driver.findElement(By.id("jqxWidgetb8f552d3")).click();
+        takeScreenShot(driver,"Popup_screen");
+        driver.findElement(By.xpath("//*[@id=\"37c88618-31a5-ec24-e465-2fe12819b248\"]")).click();
         Thread.sleep(1000);
 
-        System.out.println("After click");
-        driver.findElement(By.id("jqxWidgetb8f552d3")).sendKeys("1833");
+//        Actions actions = new Actions(driver);
+//        actions.doubleClick(driver.findElement(By.xpath("/html/body/div[1]/div/section/div/div/div/div[2]/div/div[2]/div/div[3]/div[1]/div[2]/div/div[1]/input"))).perform();
+//        Thread.sleep(1000);
+
+        //Send value 1833 to input box of BOM ID to filter BOM ID
+        driver.findElement(By.xpath("/html/body/div[1]/div/section/div/div/div/div[2]/div/div[2]/div/div[3]/div[1]/div[2]/div/div[1]/input")).sendKeys("1833");
+        Thread.sleep(3000);
+        takeScreenShot(driver,"Filter_BOM_ID");
+
+        //Double click on the record
+        Actions actions = new Actions(driver);
+        actions.doubleClick(driver.findElement(By.xpath("/html/body/div[1]/div/section/div/div/div/div[2]/div/div[2]/div/div[3]/div[2]/div/div[1]/div[1]/div"))).perform();
         Thread.sleep(1000);
+        takeScreenShot(driver,"Popup_Detailed_Record");
 
-    }
+    }//End of method getBOMId
 
+}//End of class
 
-
-}
